1a58be230a02ef4b2b06e14354e3b75e625ddc21
Jenny Cho <jenny.cho@nhn.com>
2019-06-27 12:48:04 +0900

    * fix babel-loader option in webpack config * fix eslint * add dev dependency (core-js)

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,10 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                      presets: ['@babel/preset-env', {
+                        'useBuiltIns': 'entry',
+                        'corejs': 2
+                      }]
                     }
                 }
             },
